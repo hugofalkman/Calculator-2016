fcfe1bcd4638454847325db54a121ddea867ef1e
hugofalkman <zilla@hs-macbook-pro.local>
2016-04-24 16:24:45 +0200

    End of lecture 1

--- Calculator2016/ViewController.swift
+++ Calculator2016/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsTyping = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func performOperation(sender: UIButton) {
+        userIsTyping = false
+        if let mathSymbol = sender.currentTitle {
+            if mathSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
     }
-
-
+    
+    
 }
 
